5ad0c18d2ec7506809b5732f346da1987bafcdd5
tokenicrat <tokenicrat@tuta.io>
2025-02-16 09:16:46 +0800

    Initial version, many functions missing

---
+++ conf.py
@@ -0,0 +1,4 @@
+config = {
+    "SITE_NAME": "Tokenicrat's Blog",
+    "SITE_ADDR": "https://tokeniblog-demo.pages.dev"
+}
---
+++ template.py
@@ -0,0 +1,8 @@
+template = {
+    "post": """
+
+""",
+    "front_page": """
+
+"""
+}
