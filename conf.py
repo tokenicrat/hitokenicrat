0a40450b4f8112df473de57715851cb7e21ef404
Tokenicrat <tokenicrat@tuta.io>
2025-02-16 17:17:14 +0800

    Correct base URL for demo

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 config = {
     "SITE_NAME": "MailBlog",
-    "SITE_ADDR": "https://mailblog.tokenisite.top",
+    "SITE_ADDR": "https://mailblog.pages.dev",
     "AUTHOR_NAME": "MailBlog",
     "TOOL_BAR": """
 <a href="/">Home</a> | <a href="/post/about.html">About</a> | <a href="https://github.com/tokenicrat">GitHub</a>
